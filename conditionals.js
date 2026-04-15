abfa840b9aa41230ca7f4e8ef2e08de191289057
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-15 21:37:47 +0530

    conditional-new-commit

--- conditionals.js
+++ conditionals.js
@@ -1,38 +1,103 @@
-//if
-let a = 20;
-if(a >= 18){
-    console.log(a);
-}
+// //if
+// let a = 20;
+// if(a >= 18){
+//     console.log(a);
+// }
 
-//if else 
-let s = 40;
-if(s <= 20){
-    console.log("s is less than 20");
-}else{
-    console.log("s is greater than 20");
-}
+// //if else 
+// let s = 40;
+// if(s <= 20){
+//     console.log("s is less than 20");
+// }else{
+//     console.log("s is greater than 20");
+// }
 
-//if, else if, else
-let score = 75;
-if(score >= 90){
-    console.log("Grade A");
-}else if(score >= 75){
-    console.log("Grade B");
-}else if(score >= 50){
-    console.log("Grade C");
-}else{
-    console.log("fail");
-}
+// //if, else if, else
+// let score = 75;
+// if(score >= 90){
+//     console.log("Grade A");
+// }else if(score >= 75){
+//     console.log("Grade B");
+// }else if(score >= 50){
+//     console.log("Grade C");
+// }else{
+//     console.log("fail");
+// }
 
-//Nested if 
-let age = 20;
-let HasId = true;
-if(age >= 18){
-    if(HasId){
-        console.log("Entry allowed");
-    }else{
-        console.log("Entry is denied");
-    }
-}else{
-    console.log("Not Eligible for entry");
-}
+// //Nested if 
+// let age = 20;
+// let HasId = true;
+// if(age >= 18){
+//     if(HasId){
+//         console.log("Entry allowed");
+//     }else{
+//         console.log("Entry is denied");
+//     }
+// }else{
+//     console.log("Not Eligible for entry");
+// }
+
+//ternary shorthand operator 
+// let age = 20;
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
+
+// Truthy and Falsy Values
+
+// JavaScript automatically treats some values as `true` or `false` in conditions.
+
+// **Falsy values** (treated as `false`):
+
+// ```javascript
+// false
+// 0
+// ""        // empty string
+// null
+// undefined
+// NaN
+// ```
+
+// **Truthy values** (everything else):
+
+// ```javascript
+// true
+// 1
+// "hello"
+// []        // even empty array
+// {}        // even empty object
+// ```
+
+// switch in JavaScript
+// let day = 1;
+// switch(day){
+//     case 1: 
+//           console.log("Monday");
+//           break;
+//     case 2: 
+//           console.log("Tuesday");
+//           break;
+//     case 3: 
+//           console.log("Wednesday")
+//           break; 
+//     default: 
+//           console.log("Invalid!");                
+// }
+
+// switch with return (inside a function)
+
+// When inside a function, you can use `return` instead of `break`.
+
+// ```javascript
+// function getDayType(day) {
+//     switch (day) {
+//         case "Saturday":
+//         case "Sunday":
+//             return "Weekend";
+//         default:
+//             return "Weekday";
+//     }
+// }
+
+// console.log(getDayType("Saturday")); // Weekend
+// console.log(getDayType("Monday"));   // Weekday
+
