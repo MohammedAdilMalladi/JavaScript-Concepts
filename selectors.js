13559cf0d358b51a9316c54cece2cd04b8a50dc0
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-16 18:33:55 +0530

    selectors-commit

--- selectors.js
+++ selectors.js
@@ -1,33 +1,29 @@
-// let a = document.getElementsByClassName("box"); // selects element of class by index number
-// a[0].style.backgroundColor = "red";
+let a = document.getElementsByClassName("box"); // selects element of class by index number
+a[0].style.backgroundColor = "red";
 
-// let b = document.getElementById("bluebox"); //no need to use id or class ( # and .) symbol here
-// b.style.backgroundColor = "blue";
+let b = document.getElementById("bluebox"); //no need to use id or class ( # and .) symbol here
+b.style.backgroundColor = "blue";
 
-// let c = document.querySelector("#greenbox"); // by default it selects first element
-// c.style.backgroundColor = "green";
+let c = document.querySelector("#greenbox"); // by default it selects first element
+c.style.backgroundColor = "green";
 
-// let d = document.querySelectorAll(".box").forEach((d) =>{ //we use for loop or forEach loop to select all the elements
-//      d.style.backgroundColor = "purple";
-//      d.style.color = "white";
-// })
+let d = document.querySelectorAll(".box").forEach((d) =>{ //we use for loop or forEach loop to select all the elements
+     d.style.backgroundColor = "purple";
+     d.style.color = "white";
+})
 
-// document.getElementsByTagName("div")
-// HTMLCollection(6) [div.container, div.box, div#bluebox.box, div#greenbox.box, div.box, div.box, bluebox: div#bluebox.box, greenbox: div#greenbox.box]
+document.getElementsByTagName("div")
 
 // let e = document.getElementsByTagName("div");
 // undefined
 // e
 // HTMLCollection(6) [div.container, div.box, div#bluebox.box, div#greenbox.box, div.box, div.box, bluebox: div#bluebox.box, greenbox: div#greenbox.box]
-// e[3].matches("#greenbox");
+e[3].matches("#greenbox");
 // true
-// e[2].closest("#bluebox");
-// <div id=​"bluebox" class=​"box" style=​"background-color:​ purple;​ color:​ white;​">​box 2​</div>​
-// e[0].closest(".container");
-// <div class=​"container">​…​</div>​
-// document.querySelector("#bluebox").contains(e[2]); //for contains we need to use an variable and after that we need to perform let e = document.getElementsByTagName("div"); and after that print e on console and after that perform the contains operation it will generate an error when the e is undefined
+e[2].closest("#bluebox");
+//for contains we need to use an variable and after that we need to perform let e = document.getElementsByTagName("div"); and after that print e on console and after that perform the contains operation it will generate an error when the e is undefined
 // true
-// document.querySelector(".container").contains(document.querySelector("body"));
+document.querySelector(".container").contains(document.querySelector("body"));
 // false
-// document.querySelector("body").contains(document.querySelector(".container"));
+document.querySelector("body").contains(document.querySelector(".container"));
 // true
