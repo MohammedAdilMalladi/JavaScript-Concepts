26b90678204ddfe28a7ade5b7b3b14a6afa41196
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-16 18:14:22 +0530

    formatted

--- arrays.js
+++ arrays.js
@@ -1,58 +1,58 @@
-// let arr = [1,2,3,4,5,6];
-// console.log(arr);
-// console.log(arr[0]);
-// console.log(arr[1]);
-// console.log(arr[2]);
-// console.log(arr[3]);
-// console.log(arr[4]);
-// console.log(arr[5]);
-// console.log(typeof arr); returns the type of array as object in javascript.
+let arr = [1,2,3,4,5,6];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(typeof arr); //returns the type of array as object in javascript.
 
-// console.log(arr.length);
+console.log(arr.length);
 
-// console.log(arr.toString());
+console.log(arr.toString());
 
-//let a = ["Harry", "Adil", "Rahul", "Rohan"];
-// console.log(a.join(" - "));
+let a = ["Harry", "Adil", "Rahul", "Rohan"];
+console.log(a.join(" - "));
 
-// console.log(arr.pop()); removes the element from the end and returns the popped element
-// console.log(arr);
+console.log(arr.pop()); //removes the element from the end and returns the popped element
+console.log(arr);
 
-// console.log(arr.push(100)); // adds an element at the end of the array and returns the number of elements in an array after pushing
-// console.log(arr);
-// arr.push("Adil");
-// console.log(arr);
-// console.log(arr.length);
+console.log(arr.push(100)); // adds an element at the end of the array and returns the number of elements in an array after pushing
+console.log(arr);
+arr.push("Adil");
+console.log(arr);
+console.log(arr.length);
 
 
-// arr.shift(); // removes first element from the array
-// console.log(arr);
+arr.shift(); // removes first element from the array
+console.log(arr);
 
-// arr.unshift("Adil");// adds first element in the array
-// console.log(arr);
+arr.unshift("Adil");// adds first element in the array
+console.log(arr);
 
-// delete arr[2]; // deletes element from array
-// console.log(arr);
+delete arr[2]; // deletes element from array
+console.log(arr);
 
-// let arr1 = ["Mohammed", "Adil"]; //array concatenation
-// let arr2 = ["Malladi"];
-// let arr3 = arr1.concat(arr2);
-// console.log(arr3);
+let arr1 = ["Mohammed", "Adil"]; //array concatenation
+let arr2 = ["Malladi"];
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
 
-// let arr = [6,5,4,3,1,2]; // array sorting using sort() method
-// arr.sort();
-// console.log(arr);
+let arr = [6,5,4,3,1,2]; // array sorting using sort() method
+arr.sort();
+console.log(arr);
 
-// let arr = [1,2,3,4,5,6,7]; //array reversing
-// arr.reverse();
-// console.log(arr);
+let arr = [1,2,3,4,5,6,7]; //array reversing
+arr.reverse();
+console.log(arr);
 
-// let arr = [1,2,3,4,5,6,7,8]; //splice operation performs insertion and deletion from array by specifying first the index number of then the number of deletion and insertion, incase of insertion we need to pass the two mandatory values, and after with the elements we want to add to the specific position in an array.
-// // arr.splice(3,2);
-// // console.log(arr);
-// arr.splice(3, 3, 47, 44, 55);
-// console.log(arr);
+let arr = [1,2,3,4,5,6,7,8]; //splice operation performs insertion and deletion from array by specifying first the index number of then the number of deletion and insertion, incase of insertion we need to pass the two mandatory values, and after with the elements we want to add to the specific position in an array.
+arr.splice(3,2);
+console.log(arr);
+arr.splice(3, 3, 47, 44, 55);
+console.log(arr);
 
-// const arr = [1,2,3,4,5,6]; //slices out the elements into new array or creates new array.
-// const arr2 = arr.slice(2);
-// console.log(arr2);
+const arr = [1,2,3,4,5,6]; //slices out the elements into new array or creates new array.
+const arr2 = arr.slice(2);
+console.log(arr2);
