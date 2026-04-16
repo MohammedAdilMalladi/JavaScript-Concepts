c697262583f1710806d11ad6b23b387c10d5e4eb
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-16 18:30:22 +0530

    new-commit

--- new.js
+++ new.js
@@ -1,41 +1,41 @@
-//alert("This method displays an alert message on the screen");
+alert("This method displays an alert message on the screen");
 
-//let a = prompt("Enter your name:  ");
-//console.log("Entered name is:  "+a);
+let a = prompt("Enter your name:  ");
+console.log("Entered name is:  "+a);
 
-//let isTrue = confirm("Are you sure you want to go ahead");
+let isTrue = confirm("Are you sure you want to go ahead");
 
-//if (isTrue){
-    //console.log("You are ready to go");
-//}
-//else{
-    //console.log("Please Enter the required field");
-//}
+if (isTrue){
+    console.log("You are ready to go");
+}
+else{
+    console.log("Please Enter the required field");
+}
 
-//Variables
+Variables
 
-//var a = 10;
-//var b = 20;
-//console.log(a + b + 10);
+var a = 10;
+var b = 20;
+console.log(a + b + 10);
 
-//let x = 10;
+let x = 10;
 
-//{
-    //let x = 20;
-    //console.log(x);
-//}
+{
+    let x = 20;
+    console.log(x);
+}
 
-//console.log(x);
+console.log(x);
 
-//const z = "Coder";
-//console.log(z);
+const z = "Coder";
+console.log(z);
 
-m = 15;
-n = 1.5;
-o = "Coder";
-p = true;
-q = undefined;
-r = null;
+// m = 15;
+// n = 1.5;
+// o = "Coder";
+// p = true;
+// q = undefined;
+// r = null;
 
 console.log(typeof m,typeof n,typeof o,typeof p, typeof q, typeof r);
 
