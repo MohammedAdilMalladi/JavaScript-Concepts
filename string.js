c46caf61a7abc1933abfcb1a85c718d5f7662d21
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-16 18:35:08 +0530

    strings-commit

--- string.js
+++ string.js
@@ -1,42 +1,41 @@
-// let a = "Adil";
-// console.log(a);
+let a = "Adil";
+console.log(a);
 
-// console.log(a[0]);
-// console.log(a[1]);
-// console.log(a[2]);
-// console.log(a[3]);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
 
-// console.log(a.toUpperCase());
-// console.log(a.toLowerCase());
-// console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.length);
 
-// console.log(a.slice(0, 2));
+console.log(a.slice(0, 2));
 
-// console.log(a.replace("Adil", "Abid"));
+console.log(a.replace("Adil", "Abid"));
 
-// console.log(a.concat("Nyamatulla", "Malladi"));
+console.log(a.concat("Nyamatulla", "Malladi"));
 
-// let name = "Adil";
-// let surname = "Malladi";
-// console.log("My name is " + name + " My surname is " +surname);
-// console.log(`Name is ${name} and surname is ${surname}`);
+let name = "Adil";
+let surname = "Malladi";
+console.log("My name is " + name + " My surname is " +surname);
+console.log(`Name is ${name} and surname is ${surname}`);
 
-// let firstname = "MohammedAdil";
-// let lastname = "Malladi";
-// let result = firstname.concat(lastname, "you are right");
-// console.log(result);
+let firstname = "MohammedAdil";
+let lastname = "Malladi";
+let result = firstname.concat(lastname, "you are right");
+console.log(result);
 
-// let trim = "   Whitespaces   ";
-// console.log(trim.trim());
+let trim = "   Whitespaces   ";
+console.log(trim.trim());
 
-// let c = "Code";
-// let result = c.charAt(1);
-// console.log(result);
+let c = "Code";
+let result = c.charAt(1);
+console.log(result);
 
-// b = "Books";
-// console.log(b.indexOf("ks"));
+b = "Books";
+console.log(b.indexOf("ks"));
 
-// c = "VSCode";
-// console.log(c.startsWith("V"));
-// console.log(c.endsWith("e"));
-
+c = "VSCode";
+console.log(c.startsWith("V"));
+console.log(c.endsWith("e"));
