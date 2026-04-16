26fc2eeeb90bd725fb6615880d89c0282e0eb062
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-16 15:01:39 +0530

    first-loops-commit

--- loops.js
+++ loops.js
@@ -1,31 +1,6 @@
-// let arr = [1,2,3,4,5,6];
-// for (let index = 0; index < arr.length; index++) { //for loop
-//     const element = arr[index];
-//     console.log(index, arr);
-    
-// }
+// A loop is used to execute a block of code repeatedly until a condition is met.
 
-// let obj = {
-//     a: 1,
-//     b: 2,
-//     c: 3,
-//     a: "2",
-//     b: 7
-// }
-
-// for (const key in obj) { //for in loop is used with objects
-//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-//         const element = obj[key];
-//         console.log(key, element);
-        
-//     }
-// }
-
-// let a = "AdilMalladi"; 
-// for (const element of a) { for of loop displays output in vertical manner
-//     console.log(element);
-// }
-
-// arr.forEach((value, index, arr)=>{ forEach loop displays values with index and with also the complete array.
-//     console.log(value, index, arr);
-// })
+// for loop
+for(let i = 1; i <= 3; i++){
+    console.log(i);
+}
