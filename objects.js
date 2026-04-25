5aeaee80c5564c496bcf5e05a58268695c158750
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-25 23:21:54 +0530

    accessing-of-objects

--- objects.js
+++ objects.js
@@ -1,8 +1,12 @@
-let obj = {
+let person = {
     name: "Adil",
     age: 23
 };
 
-for(let key in obj){
-    console.log(key, obj[key]);
-}
+// for(let key in obj){
+//     console.log(key, obj[key]);
+// }
+
+//accessing object values
+console.log(person.name); //Adil
+console.log(person.age); //23
