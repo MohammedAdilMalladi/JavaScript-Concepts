bd1c9ac05137cfdb316145f16a85f165bad6e7c2
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-04-30 23:16:00 +0530

    updated js concepts

--- DOM-Concepts/dom.js
+++
@@ -1,2 +0,0 @@
-// What is DOM - DOM stands for Document object model It is a programming interface that allows JavaScript to access HTML elements, change content, change styles, handle events
-// DOM connects JavaScript + HTML
