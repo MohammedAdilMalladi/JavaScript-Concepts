d57d712c461febd4c9f628819a0ff1e1572a30c4
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-01 00:05:00 +0530

    Removed unnecessary files

--- dom.js
+++
@@ -1,7 +0,0 @@
-// What is DOM - DOM stands for Document object model It is a programming interface that allows JavaScript to access HTML elements, change content, change styles, handle events
-// DOM connects JavaScript + HTML
-
-//Selecting elements
-// 1. getElementById() - Selects an element using its id
-let h1 = document.getElementById("header");
-console.log(h1);
