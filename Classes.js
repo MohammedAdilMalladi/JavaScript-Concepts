f2ac8926fe6e29ef5ccd6407f01d127a111f7fc2
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-03 23:49:45 +0530

    started with OOPs in JS

--- Classes.js
+++ Classes.js
@@ -1,18 +1,35 @@
-class LandRover{
-    constructor(brand){
-        console.log("Constructor will be invoked automatically"); //Constructor will be invoked automatically
-        this.brand = brand;
-    }
+// class LandRover{
+//     constructor(brand){
+//         console.log("Constructor will be invoked automatically"); //Constructor will be invoked automatically
+//         this.brand = brand;
+//     }
 
-    startengine(){
-        console.log("Engine started"); //Engine started
-    }
+//     startengine(){
+//         console.log("Engine started"); //Engine started
+//     }
 
-    stop(){
-        console.log("Stop"); //Stop
+//     stop(){
+//         console.log("Stop"); //Stop
+//     }
+// }
+
+// let Defender = new LandRover("Defender");
+// Defender.startengine();
+// Defender.stop();
+
+// inheritance
+class Base{
+    parent(){
+        console.log("this is parent class"); //this is parent class
     }
 }
 
-let Defender = new LandRover("Defender");
-Defender.startengine();
-Defender.stop();
+class Derived extends Base{
+    child(){
+        console.log("This is child class"); //this is chid class
+    }
+}
+
+let d = new Derived();
+d.parent();
+d.child();
