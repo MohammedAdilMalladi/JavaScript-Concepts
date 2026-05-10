c57463440d3a4cb84f17bd47867149b9ab92a175
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-10 13:46:07 +0530

    Completed with API

--- API-JS/app.js
+++ API-JS/app.js
@@ -4,19 +4,45 @@
 
 
 const url = "https://official-joke-api.appspot.com/random_joke";
-const jokes = document.querySelector("#jokes");
 const btn = document.querySelector("#btn");
+const jokesContainer = document.getElementById("jokesContainer");
 
+//API Calling using async await
 const getJokes = async () => {
-    console.log("fetching data......")
+    console.log("fetching data......");
     let response = await fetch(url);
     console.log(response);
     // console.log(response.status); //200
     let data = await response.json();
-    console.log(data); // returns data in a usable format 
+    console.log(data); // returns data in a usable and readable format 
     // console.log(data[0]);
     // console.log(data[0].text);
-    jokes.innerText = data.setup; //What do you call a fly without wings?
+    let setup = document.createElement("p");
+    console.log(setup.innerText = "Setup: " + data.setup);
+    let punchline = document.createElement("p");
+    console.log(punchline.innerText = "Punchline: " + data.punchline);
+
+    jokesContainer.append(setup);
+    jokesContainer.append(punchline);
 }
 
+//API calling using promise chaining
+function getJokesp() {
+    fetch(url).then((res) => {
+        console.log("fetching data......");
+        console.log(res);
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        let setup = document.createElement("p");
+        console.log(setup.innerText = "Setup: " + data.setup);
+        let punchline = document.createElement("p");
+        console.log(punchline.innerText = "Punchline: " + data.punchline);
+
+        jokesContainer.append(setup);
+        jokesContainer.append(punchline);
+    });
+};
+
 btn.addEventListener("click", getJokes);
+btn.addEventListener("click", getJokesp);
