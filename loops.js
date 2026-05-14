c232b3749b27e9825dbf3689db29545065cf9c4e
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-14 23:52:52 +0530

    comments-commit

--- loops.js
+++ loops.js
@@ -52,7 +52,7 @@ for(let i = 1; i <= 5; i++){
     console.log(i);
 }
 
-// // Nested Loops
+// // Nested Loops - A loop inside another loop is called as nested loops
 let rows = 5;
 for(let i = 1; i <= rows; i++){
     for(let j = 1; j <= i; j++){
